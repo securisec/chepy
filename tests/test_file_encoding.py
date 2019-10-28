36c7dd6e05fe056519c3f0764679cf6181a497fc
0xJohannes <43135084+0xJohannes@users.noreply.github.com>
2019-10-28 21:00:08 +0400

    low_brightness new method (#1)
    * low_brightness new method
    
    * low_brightness add base58 decode test case
    
    * low_brightness add docs
    
    * low_brightness add docs for decode

--- tests/test_file_encoding.py
+++ tests/test_file_encoding.py
@@ -32,6 +32,8 @@ def test_rot_47():
 def test_base_64_encode():
     assert Chepy("some data").base_64_encode.output.decode() == "c29tZSBkYXRh"
 
+def test_base_58_encode():
+    assert Chepy("some data").base_58_encode.output.decode() == "2UDrs31qcWSPi"
 
 def test_base_32_encode():
     assert Chepy("some data").base_32_encode.output.decode() == "ONXW2ZJAMRQXIYI="
@@ -47,3 +49,6 @@ def test_to_hex():
 
 def test_hex_to_int():
     assert Chepy("0x123").hex_to_int.output == 291
+
+def test_base_58_decode():
+    assert Chepy("2UDrs31qcWSPi").base_58_decode.output.decode() == "some data"
