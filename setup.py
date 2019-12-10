b0c965c6124fcb64df916ba5ef8b9baa734758e2
securisec <securisecctf@gmail.com>
2019-12-09 20:16:32 -0500

    🐍 ℹ️
    🐍 added python-magic as deps
    ℹ️ updated setup.py to handle this dep depending on os

--- setup.py
+++ setup.py
@@ -1,27 +1,9 @@
 # pylint: disable=undefined-variable
+import sys
 from setuptools import setup, find_packages
 from os import path
 from shutil import copy
 from pathlib import Path
-from configparser import ConfigParser
-
-# create .chefy dir in home and move base conf file if it does not exist
-home = Path.home()
-chepy_dir = Path(home / ".chepy")
-chepy_conf = Path(chepy_dir / "chepy.conf")
-rc_path = Path("chepy.conf").absolute()
-
-Path(chepy_dir).mkdir(exist_ok=True)
-# Chepy default configs
-config = ConfigParser()
-config.read(str(rc_path))
-# history file
-config["Cli"]["HistoryPath"] = str(chepy_dir / "chepy_history")
-Path(str(chepy_dir / "chepy_history")).touch(exist_ok=True)
-# if file already exists, do not overwrite it
-if not chepy_conf.exists():
-    with open(str(chepy_conf), "w") as f:
-        config.write(f)
 
 # get version and author information
 with open("chepy/__version__.py", "r") as f:
@@ -32,6 +14,12 @@ def read_requirements():
     with open("requirements.txt") as f:
         return f.read().splitlines()
 
+
+requirements = read_requirements()
+if sys.platform == "linux":
+    requirements.append("python-magic")
+else:
+    requirements.append("python-magic-bin==0.4.14")
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md")) as f:
@@ -44,13 +32,13 @@ setup(
     license="GPL",
     version=__version__,
     author=__author__,
-    url='https://github.com/securisec/chepy',
+    url="https://github.com/securisec/chepy",
     project_urls={
         "Documentation": "https://chepy.readthedocs.io/en/latest/",
         "Source Code": "https://github.com/securisec/chepy",
     },
     packages=find_packages(exclude=(["tests", "docs"])),
-    install_requires=read_requirements(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
